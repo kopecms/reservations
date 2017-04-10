667e2addacb55f55be4652f080b101084ac475c8
kopec <kopecms@gmail.com>
2017-04-10 12:59:19 +0200

    podpiecie bazy danych

---
+++ src/main/java/app/controllers/UserController.java
@@ -0,0 +1,27 @@
+package app.controllers;
+
+import java.util.List;
+
+import app.repositories.UserRepository;
+import app.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping(value="", method=RequestMethod.GET)
+    public List<User> getAllUsers() {
+        return userRepository.getAll();
+    }
+}
